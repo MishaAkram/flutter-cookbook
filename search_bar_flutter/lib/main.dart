a03405e23b4fc5f9295ecebc2cc332e43ee36342
MishaAkram <mishaakrambaig@gmail.com>
2021-10-24 00:41:52 +0500

    searchbar code added

--- search_bar_flutter/lib/main.dart
+++ search_bar_flutter/lib/main.dart
@@ -1,115 +1,161 @@
+import 'country_model.dart';
 import 'package:flutter/material.dart';
+import 'package:searchfield/searchfield.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: "Demo Home Page",
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    countries = data.map((e) => Country.fromMap(e)).toList();
+  }
+
+  List<Country> countries = [];
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+            ),
+            body: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SearchField(
+                    suggestions: countries.map((e) => e.name).toList(),
+                    suggestionState: SuggestionState.enabled,
+                    controller: _searchController,
+                    hint: 'Search by country name',
+                    maxSuggestionsInViewPort: 4,
+                    itemHeight: 45,
+                    onTap: (x) {
+                      final country = countries.firstWhere((e) => e.name == x);
+                      Navigator.of(context)
+                          .push(MaterialPageRoute<CountryDetail>(
+                              builder: (_) => CountryDetail(
+                                    country: country,
+                                  )));
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: ListView.separated(
+                    itemCount: countries.length,
+                    separatorBuilder: (_, index) => Divider(),
+                    itemBuilder: (_, index) => ListTile(
+                      title: Text(countries[index].name),
+                      trailing: IconButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .push(MaterialPageRoute<CountryDetail>(
+                                    builder: (_) => CountryDetail(
+                                          country: countries[index],
+                                        )));
+                          },
+                          icon: Icon(Icons.arrow_forward_ios, size: 16)),
+                    ),
+                  ),
+                )
+              ],
+            )));
+  }
+}
+
+class CountryDetail extends StatefulWidget {
+  final Country country;
+
+  const CountryDetail({Key? key, required this.country}) : super(key: key);
+  @override
+  _CountryDetailState createState() => _CountryDetailState();
+}
+
+class _CountryDetailState extends State<CountryDetail> {
+  Widget dataWidget(String key, int value) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.15),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("$key:"),
+          const SizedBox(
+            width: 16,
+          ),
+          Flexible(
+            child: Text(
+              '$value',
+              style: const TextStyle(fontSize: 30),
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Details'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            alignment: Alignment.center,
+            child: Text(
+              widget.country.name,
+              style: const TextStyle(fontSize: 40),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          dataWidget('Population:', widget.country.population),
+          dataWidget('Density', widget.country.density),
+          dataWidget('Land Area (in Km\'s)', widget.country.landArea)
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
